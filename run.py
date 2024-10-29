a0d67f1c670c9c0057391fd815e501986bc2042a
Xiaowen.Z <xiaowen.z@outlook.com>
2024-10-29 09:55:37 +0800

    feat: add more stories

--- run.py
+++ run.py
@@ -196,8 +196,8 @@ def batch_process(from_date=datetime.now(), to_date=datetime.now()):
 
 
 if __name__ == "__main__":
-    str_from = "2024-10-01"
-    str_to = "2024-10-24"
+    str_from = "2024-10-29"
+    str_to = "2024-10-29"
     from_date=datetime.strptime(str_from, '%Y-%m-%d')
     to_date=datetime.strptime(str_to, '%Y-%m-%d')
 
